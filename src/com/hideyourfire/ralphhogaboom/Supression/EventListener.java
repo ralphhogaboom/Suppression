5f968910cccba912fc8a86449a36a9fc892d67e1
ralphhogaboom <ralph@hogaboom.org>
2014-11-30 09:01:49 -0800

    Removed the console logger when players successfully place lava buckets, lighters, firecharges.

--- src/com/hideyourfire/ralphhogaboom/Supression/EventListener.java
+++ src/com/hideyourfire/ralphhogaboom/Supression/EventListener.java
@@ -17,9 +17,9 @@ public class EventListener implements Listener {
 		Player player = event.getPlayer();
 		if (player.getItemInHand().getTypeId() == Material.LAVA_BUCKET.getId()) {
 			if (player.hasPermission("Suppression.Use")) {
-				Main.getPlugin().getLogger().info(player.getDisplayName() + " placed lava bucket.");
+				// Main.getPlugin().getLogger().info(player.getDisplayName() + " placed lava bucket.");
 			} else {
-				Main.getPlugin().getLogger().info(player.getDisplayName() + " lava bucket place cancelled.");
+				Main.getPlugin().getLogger().info("[Suppression INFO]: " + player.getDisplayName() + " lava bucket place cancelled.");
 				event.setCancelled(true);
 			}
 		}
@@ -31,9 +31,9 @@ public class EventListener implements Listener {
 		Player player = event.getPlayer();
 		if (player.getItemInHand().getTypeId() == Material.FIREWORK_CHARGE.getId()) {
 			if (player.hasPermission("Suppression.Use")) {
-				Main.getPlugin().getLogger().info(player.getDisplayName() + " used a firecharge." );
+				// Main.getPlugin().getLogger().info(player.getDisplayName() + " used a firecharge." );
 			} else {
-				Main.getPlugin().getLogger().info(player.getDisplayName() + " firecharge use cancelled.");
+				Main.getPlugin().getLogger().info("[Suppression INFO]: " + player.getDisplayName() + " firecharge use cancelled.");
 				event.setCancelled(true);
 			}
 		}
@@ -45,9 +45,9 @@ public class EventListener implements Listener {
 		Player player = event.getPlayer();
 		if (player.getItemInHand().getTypeId() == Material.FLINT_AND_STEEL.getId()) {
 			if (player.hasPermission("Suppression.Use")) {
-				Main.getPlugin().getLogger().info(player.getDisplayName() + " used a lighter." );
+				// Main.getPlugin().getLogger().info(player.getDisplayName() + " used a lighter." );
 			} else {
-				Main.getPlugin().getLogger().info(player.getDisplayName() + " lighter use cancelled.");
+				Main.getPlugin().getLogger().info("[Suppression INFO]: " + player.getDisplayName() + " lighter use cancelled.");
 				event.setCancelled(true);
 			}
 		}
