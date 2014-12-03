b6b3a4f5f38ba95d02375d54887775e6b2dc0e24
ralphhogaboom <ralph@hogaboom.org>
2014-12-03 08:52:41 -0800

    Removed unused dependencies, updated version number.

--- src/com/hideyourfire/ralphhogaboom/Supression/EventListener.java
+++ src/com/hideyourfire/ralphhogaboom/Supression/EventListener.java
@@ -4,50 +4,49 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.player.PlayerBucketEmptyEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class EventListener implements Listener {
 
 	@SuppressWarnings("deprecation")
+
 	@EventHandler(ignoreCancelled = true)
 	public void onPlayerBucket(PlayerBucketEmptyEvent event)
 	{
 		Player player = event.getPlayer();
 		if (player.getItemInHand().getTypeId() == Material.LAVA_BUCKET.getId()) {
-			if (player.hasPermission("Suppression.Use")) {
-				// Main.getPlugin().getLogger().info(player.getDisplayName() + " placed lava bucket.");
-			} else {
+			if (!(player.hasPermission("Suppression.Use"))) {
 				Main.getPlugin().getLogger().info("[Suppression INFO]: " + player.getDisplayName() + " lava bucket place cancelled.");
+				player.sendMessage("You don't have permission to use a lava bucket.");
 				event.setCancelled(true);
 			}
 		}
 	}
 
+	@SuppressWarnings("deprecation")
 	@EventHandler(ignoreCancelled = true)
 	public void onPlayerFirecharge(PlayerInteractEvent event)
 	{
 		Player player = event.getPlayer();
-		if (player.getItemInHand().getTypeId() == Material.FIREWORK_CHARGE.getId()) {
-			if (player.hasPermission("Suppression.Use")) {
-				// Main.getPlugin().getLogger().info(player.getDisplayName() + " used a firecharge." );
-			} else {
+		if (player.getItemInHand().getTypeId() == 385) {
+			if (!(player.hasPermission("Suppression.Use"))) {
 				Main.getPlugin().getLogger().info("[Suppression INFO]: " + player.getDisplayName() + " firecharge use cancelled.");
+				player.sendMessage("You don't have permission to use a fire charge.");
 				event.setCancelled(true);
 			}
 		}
 	}	
 	
+	@SuppressWarnings("deprecation")
 	@EventHandler(ignoreCancelled = true)
 	public void onPlayerLighter(PlayerInteractEvent event)
 	{
 		Player player = event.getPlayer();
 		if (player.getItemInHand().getTypeId() == Material.FLINT_AND_STEEL.getId()) {
-			if (player.hasPermission("Suppression.Use")) {
-				// Main.getPlugin().getLogger().info(player.getDisplayName() + " used a lighter." );
-			} else {
+			if (!(player.hasPermission("Suppression.Use"))) {
 				Main.getPlugin().getLogger().info("[Suppression INFO]: " + player.getDisplayName() + " lighter use cancelled.");
+				player.sendMessage("You don't have permission to use flint and steel.");
 				event.setCancelled(true);
 			}
 		}
--- src/com/hideyourfire/ralphhogaboom/Supression/Main.java
+++ src/com/hideyourfire/ralphhogaboom/Supression/Main.java
@@ -1,23 +1,8 @@
 package com.hideyourfire.ralphhogaboom.Supression;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Random;
-import java.util.logging.Level;
-
 import org.bukkit.Bukkit;
-import org.bukkit.block.Block;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
